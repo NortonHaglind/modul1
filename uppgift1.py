5add76ae6c71072d8304682648072698f1539436
NortonHaglind <norton.haglindhilbers@elev.ga.ntig.se>
2023-08-29 09:50:57 +0200

    program

---
+++ uppgift1.py
@@ -0,0 +1,18 @@
+print("hello world")
+'''
+lorem ipsum 
+lorem ipsum 
+lorem ipsum 
+lorem ipsum 
+lorem ipsum 
+lorem ipsum 
+lorem ipsum 
+lorem ipsum 
+lorem ipsum 
+'''
+print("haha\nhhq\nhahdbuiau\nofouaf\nhhd")
+print("failhfihaf"+"163613lff.fm")
+x= 2 
+print("fhahofq", x)
+x1= input("vad heter du?: ")
+print("hej " + x1 + " välkomen till mitt program")
